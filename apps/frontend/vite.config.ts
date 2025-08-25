457ea056bce37fa5f2e12ee149a16e508b676bf6
Richard Everley <hello@richardeverley.com>
2025-08-25 18:11:58 +1000

    - Feature: Add Yarn Type Check

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -5,21 +5,22 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: env.BACKEND_API_URL,
-        changeOrigin: true,
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
-}});
+    server: {
+      proxy: {
+        "/api": {
+          target: env.BACKEND_API_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
+});
