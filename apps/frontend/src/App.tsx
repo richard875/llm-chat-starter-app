d851f280758f790c3ebc9d0b97d08989916b4d18
Richard Everley <hello@richardeverley.com>
2025-08-25 15:53:21 +1000

    - Feature: Modify App Component

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,14 +1,17 @@
 import { Chat } from "@/components/chat/chat";
+import logo from "../public/logo.svg";
 
 const App = () => {
   return (
     <div className="flex flex-col h-screen">
-      <header className="border-b bg-background flex items-center justify-center h-14 w-full flex-none">
-        <h1 className="text-lg font-semibold">
-          LLM Chat{" "}
-          <span className="text-sm text-muted-foreground">v1.0</span>
-        </h1>
+      <header className="w-full h-14 flex items-center justify-between bg-[#a3e636] pt-2 px-5 select-none">
+        <img src={logo} alt="Logo" className="h-5" />
+        <p className="text-lg font-medium">
+          LLM Chat <span className="text-sm font-normal">v1.0</span>
+        </p>
+        <div className="w-33"></div>
       </header>
+      <div className="h-3 bg-[url('/wave.svg')] bg-left-top bg-auto"></div>
       <main className="flex-1 min-h-0">
         <Chat />
       </main>
