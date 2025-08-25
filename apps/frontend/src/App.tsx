9a5ab16bb32e398c78e7a849fb2f579a0d4d70bd
Richard Everley <hello@richardeverley.com>
2025-08-25 23:15:18 +1000

    - Styling: Adjust header items positioning

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -6,13 +6,11 @@ const App = () => {
   return (
     <div className="flex flex-col h-screen">
       <header className="w-full h-14 flex items-center justify-between bg-[#a3e636] pt-2 px-5 select-none">
-        <div className="w-60">
-          <img src={logo} alt="Logo" className="h-5" />
-        </div>
+        <img src={logo} alt="Logo" className="h-5" />
         <p className="text-lg font-medium">
           LLM Chat <span className="text-sm font-normal">v1.0</span>
         </p>
-        <div className="hidden md:block"></div>
+        <div className="w-33 hidden md:block"></div>
       </header>
       <div className="h-3 bg-[url('/wave.svg')] bg-left-top bg-auto z-20"></div>
       <main className="flex-1 min-h-0 -mt-3 flex">
