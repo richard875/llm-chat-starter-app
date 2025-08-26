dd45fa13e0d3437c947e92697f081665e71e5b22
Richard Everley <hello@richardeverley.com>
2025-08-26 10:05:42 +1000

    - Feature: App URL to frontend

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, __dirname, "");
 
   return {
     plugins: [react(), tailwindcss()],
